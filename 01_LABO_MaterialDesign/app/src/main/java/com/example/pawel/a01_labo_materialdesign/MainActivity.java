2483e54a6ea61e10ef3ebbcb44e4482eb96b90e2
Unknown <pjja14@gmail.com>
2016-11-27 18:51:19 +0100

    Material Design

---
+++ 01_LABO_MaterialDesign/app/src/main/java/com/example/pawel/a01_labo_materialdesign/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.pawel.a01_labo_materialdesign;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioButton jednaStrona;
+    private RadioButton obieStrony;
+    private EditText dataPowrotu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.search);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Czekaj, trwa wyszukiwanie polaczen....", Snackbar.LENGTH_LONG)
+                        .setAction("Szukaj", null).show();
+            }
+        });
+
+
+        jednaStrona = (RadioButton) findViewById(R.id.radioButton);
+        obieStrony = (RadioButton) findViewById(R.id.radioButton2);
+        dataPowrotu = (EditText) findViewById(R.id.powrot);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void hello(View v) {
+
+
+        if(jednaStrona.isChecked())
+        {
+
+            dataPowrotu.setText("");
+            dataPowrotu.setFocusable(false);
+        }
+        else if(obieStrony.isChecked())
+        {
+
+            dataPowrotu.setFocusableInTouchMode(true);
+        }
+
+    }
+}
